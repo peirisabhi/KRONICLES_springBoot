9a2d146a36e34e01665980629822158638b06b26
wasatTheekshana <wasathkaltheekshana@gmail.com>
2021-03-31 20:18:50 +0530

    category controller and service

--- src/main/java/com/kronicles/kronicles_springBoot/controller/UserController.java
+++ src/main/java/com/kronicles/kronicles_springBoot/controller/UserController.java
@@ -6,6 +6,7 @@ import com.kronicles.kronicles_springBoot.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -16,13 +17,24 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path = "/getAllUSer")
+    @GetMapping
     public List<User> getAllUsers(){
         return userService.getAllUsers();
     }
-    @PostMapping("/addUser")
-    public User saveUsers(@RequestBody User users){
-        return userService.addUser(users);
+
+    @PostMapping
+    public HashMap<String, Object> saveUsers(@RequestBody User users){
+//        return userService.addUser(users);
+
+        User user1 = userService.addUser(users);
+
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("status", 200);
+        response.put("data", user1);
+        response.put("message", "Success");
+
+        return response;
+
     }
 
 
@@ -31,7 +43,7 @@ public class UserController {
         return userService.deleteproductbyid(id);
     }
 
-    @PutMapping("/updateUser")
+    @PutMapping
     public User updateUser(@RequestBody User user){
         return userService.updateUser(user);
     }
