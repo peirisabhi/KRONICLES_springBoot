59351021b6bb17e3f33862b3606bf7de3ec5d63f
wasatTheekshana <wasathkaltheekshana@gmail.com>
2021-03-31 19:45:44 +0530

    user controller and service

--- src/main/java/com/kronicles/kronicles_springBoot/Services/UserService.java
+++ src/main/java/com/kronicles/kronicles_springBoot/Services/UserService.java
@@ -1,4 +1,59 @@
 package com.kronicles.kronicles_springBoot.Services;
 
+import com.kronicles.kronicles_springBoot.Repositories.UserRepo;
+import com.kronicles.kronicles_springBoot.model.Product;
+import com.kronicles.kronicles_springBoot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepo userRepo;
+
+    public List<User> getAllUsers(){
+        return userRepo.findAll();
+    }
+
+    /*
+     * Delete
+     * */
+    public String deleteproductbyid(int id) {
+        userRepo.deleteById(id);
+        return "Deleted";
+    }
+
+
+
+    /*
+     * Update
+     * */
+    public User updateUser(User u) {
+        User userreUpdate = userRepo.findById(u.getId()).orElse(null);
+        if (userreUpdate != null) {
+            userreUpdate.setName(u.getName());
+            userreUpdate.setPassword(u.getPassword());
+            userreUpdate.setAge(u.getAge());
+            userreUpdate.setTelephone(u.getTelephone());
+            userreUpdate.setUsername(u.getUsername());
+        }
+        return userreUpdate;
+    }
+
+    /*
+     * Insert
+     * */
+    public List<User> addUsers(List<User> user) {
+        return userRepo.saveAll(user);
+    }
+
+    public User addUser(User prod) {
+        return userRepo.save(prod);
+    }
+
+
+
 }
--- src/main/java/com/kronicles/kronicles_springBoot/controller/UserController.java
+++ src/main/java/com/kronicles/kronicles_springBoot/controller/UserController.java
@@ -1,6 +1,39 @@
 package com.kronicles.kronicles_springBoot.controller;
 
 
+import com.kronicles.kronicles_springBoot.Services.UserService;
+import com.kronicles.kronicles_springBoot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/users")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping(path = "/getAllUSer")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+    @PostMapping("/addUser")
+    public User saveUsers(@RequestBody User users){
+        return userService.addUser(users);
+    }
+
+
+    @DeleteMapping("{id}")
+    public String deleteUser(@PathVariable int id){
+        return userService.deleteproductbyid(id);
+    }
+
+    @PutMapping("/updateUser")
+    public User updateUser(@RequestBody User user){
+        return userService.updateUser(user);
+    }
 
 }
