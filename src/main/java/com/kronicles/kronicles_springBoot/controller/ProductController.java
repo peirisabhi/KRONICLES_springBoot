9a238564e43bc86b1a22a53e10b128d12bdde986
GrAYAtMeRzOOm <44991545+GrAYAtMeRzOOm@users.noreply.github.com>
2021-03-31 19:18:26 +0530

    updated ProductService to controller

--- src/main/java/com/kronicles/kronicles_springBoot/controller/ProductController.java
+++ src/main/java/com/kronicles/kronicles_springBoot/controller/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "*",allowedHeaders = "*")
 @RequestMapping(path = "/products")
-public class ProductService {
+public class ProductController {
 
     @Autowired
     ProductRepo proRepo;
