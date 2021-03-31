4dd649b1a7b3eb278b750004fe01adde4b67fac7
Abhishek Peiris <abhishekpeiris9@gmail.com>
2021-03-31 15:48:36 +0530

    Initial commit

---
+++ src/main/java/com/kronicles/kronicles_springBoot/KroniclesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kronicles.kronicles_springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KroniclesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KroniclesSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kronicles/kronicles_springBoot/KroniclesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kronicles.kronicles_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KroniclesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
